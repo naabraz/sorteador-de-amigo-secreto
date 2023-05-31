05ae4a0e21786a14161e828c2f06dcd965d63511
Natalia Braz <naabraz@gmail.com>
2023-05-31 08:58:08 -0300

    chore: adjust global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
 import Header from './componentes/Header';
 
-function App() {
-  return <Header />;
-}
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Header />
+  </>
+);
 
 export default App;
