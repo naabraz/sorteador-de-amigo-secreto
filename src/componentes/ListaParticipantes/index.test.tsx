a62644cd39ac151eecd25e24b341dbd193e1384d
Natalia Braz <naabraz@gmail.com>
2023-06-01 09:46:09 -0300

    create lista participantes component

---
+++ src/componentes/ListaParticipantes/index.test.tsx
@@ -0,0 +1,47 @@
+import { RecoilRoot } from 'recoil';
+import { render, screen } from '@testing-library/react';
+
+import ListaParticipantes from '.';
+import { useListaDeParticipantes } from '../../state/hooks/useListaDeParticipantes';
+
+jest.mock('../../state/hooks/useListaDeParticipantes', () => {
+  return {
+    useListaDeParticipantes: jest.fn(),
+  };
+});
+
+describe('uma lista vazia de participantes', () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue([]);
+  });
+
+  test('deve ser renderizada sem elementos', () => {
+    render(
+      <RecoilRoot>
+        <ListaParticipantes />
+      </RecoilRoot>
+    );
+
+    const itens = screen.queryAllByRole('listitem');
+    expect(itens).toHaveLength(0);
+  });
+});
+
+describe('uma lista preenchida de participantes', () => {
+  const participantes = ['Ana', 'Paula'];
+
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+  });
+
+  test('deve ser renderizada sem elementos', () => {
+    render(
+      <RecoilRoot>
+        <ListaParticipantes />
+      </RecoilRoot>
+    );
+
+    const itens = screen.queryAllByRole('listitem');
+    expect(itens).toHaveLength(participantes.length);
+  });
+});
