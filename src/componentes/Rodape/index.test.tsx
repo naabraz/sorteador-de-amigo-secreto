3e0730d0cd4a2403d17b943f24791ff785142107
Natalia Braz <naabraz@gmail.com>
2023-06-02 09:11:23 -0300

    add footer

---
+++ src/componentes/Rodape/index.test.tsx
@@ -0,0 +1,74 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+
+import { useListaDeParticipantes } from '../../state/hooks/useListaDeParticipantes';
+import Rodape from '.';
+
+jest.mock('../../state/hooks/useListaDeParticipantes', () => {
+  return {
+    useListaDeParticipantes: jest.fn(),
+  };
+});
+
+const mockNavegacao = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useNavigate: () => mockNavegacao,
+  };
+});
+
+describe('quando não existem participantes suficientes', () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue([]);
+  });
+
+  test('a brincadeira não pode ser iniciada', () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    );
+
+    const botao = screen.getByRole('button');
+
+    expect(botao).toBeDisabled();
+  });
+});
+
+describe('quando existem participantes suficientes', () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue([
+      'Ana',
+      'Paula',
+      'Jose',
+    ]);
+  });
+
+  test('a brincadeira pode ser iniciada', () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    );
+
+    const botao = screen.getByRole('button');
+
+    expect(botao).not.toBeDisabled();
+  });
+
+  test('a brincadeira foi iniciada', () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    );
+
+    const botao = screen.getByRole('button');
+
+    fireEvent.click(botao);
+
+    expect(mockNavegacao).toHaveBeenCalledTimes(1);
+    expect(mockNavegacao).toHaveBeenCalledWith('/sorteio');
+  });
+});
---
+++ src/componentes/Rodape/index.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { useListaDeParticipantes } from '../../state/hooks/useListaDeParticipantes';
+
+const Rodape = () => {
+  const participantes = useListaDeParticipantes();
+  const navegarPara = useNavigate();
+
+  const iniciar = () => {
+    navegarPara('/sorteio')
+  };
+
+  return (
+    <footer>
+      <button disabled={participantes.length < 3} onClick={iniciar}>
+        Iniciar brincadeira!
+      </button>
+    </footer>
+  );
+};
+
+export default Rodape;
